d7dc5683402a1b9587abda437924d43150b97764
Megalamanokaran <megalamanokaran26@gmail.com>
2025-03-06 20:44:08 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,9 @@
 package com.example;
 
 public class Main {
-    public void printEvenNumbers() {
-        System.out.print("Even numbers: ");
+   public void printEvenNumbers() {
+    System.out.println("numbers: [2 4 6 8 10]");
+
         for (int i = 2; i <= 10; i += 2) {
             System.out.print(i + " ");
         }
