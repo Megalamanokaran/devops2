8c2f51843559efbed9bdb13a1fc357fc0e0273b6
Megalamanokaran <megalamanokaran26@gmail.com>
2025-03-06 20:37:10 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,11 +2,16 @@
 package com.example;
 
 public class Main {
-    public String greet() {
-        return "Hello, Jenkins!"; 
+    public void printEvenNumbers() {
+        System.out.print("Even numbers: ");
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // Move to the next line after printing numbers
     }
 
     public static void main(String[] args) {
-        System.out.println(new Main().greet()); 
+        Main obj = new Main();
+        obj.printEvenNumbers();
     }
 }
