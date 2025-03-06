fb3893e1fefcfd54ff9cd7e739bf7ca71ad5e861
Megalamanokaran <megalamanokaran26@gmail.com>
2025-03-06 20:53:09 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,12 @@
-public void printEvenNumbers() {
-    System.out.print("numbers: [");
-    for (int i = 2; i <= 10; i += 2) {
-        if (i > 2) System.out.print(" "); // Add space between numbers
-        System.out.print(i);
+package com.example;
+
+public class Main {
+    public void printEvenNumbers() {
+        System.out.println("numbers: 2 4 6 8 10");
     }
-    System.out.println("]"); // Closing bracket
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printEvenNumbers();
+    }
 }
